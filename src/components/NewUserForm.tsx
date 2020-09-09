3cda5fb139af68109bcaf3f8ee734afbe340b35b
Matias Cejas <matiascejas8@gmail.com>
2020-09-09 09:27:42 -0300

    Initial setup

---
+++ src/components/NewUserForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NewUserForm = () => (
+  <div>
+    <h1>test</h1>
+    form
+  </div>
+);
+
+export default NewUserForm;
---
+++ src/components/UserList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UserList = () => (
+  <div>
+    <h1>test</h1>
+    test
+  </div>
+);
+
+export default UserList;
